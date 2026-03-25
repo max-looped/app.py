984b64838bdde14b4b3154e9eaf0c1682c3ff63e
max-looped <maxlade.loopedgaming@gmail.com>
2026-03-25 17:54:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope = ["https://spreadsheets.google.com/feeds", 
          "https://www.googleapis.com/auth/drive"]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+import streamlit as st
+
+creds = ServiceAccountCredentials.from_json_keyfile_dict(
+    st.secrets["gcp_service_account"], scope
+)
 client = gspread.authorize(creds)
 
 sheet = client.open("YOUR SHEET NAME").sheet1
